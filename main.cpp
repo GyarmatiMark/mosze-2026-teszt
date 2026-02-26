3e19681fe7f31ca8750f42ae66f0a9ec12529325
GyarmatiMark <gyarmati.mark22@gmail.com>
2026-02-26 20:58:56 +0100

    a copilot által nem generált comment
    hibák kommentezése

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // hiányzó "_"
+    std::cout << '1-100 ertekek duplazasa' // '' helyett ""
+    for (int i = 0;) //hiányos ciklusmag
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //hiányzó ";"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i]
+        atlag += b[i] //hiányzó ";"
     }
-    atlag /= N_ELEMENTS;
+    atlag /= N_ELEMENTS; 
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
 }
+
