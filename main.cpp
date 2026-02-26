817cc21ec47dccabb4fb726c1b3ef8348907ffc5
GyarmatiMark <gyarmati.mark22@gmail.com>
2026-02-26 21:01:36 +0100

    ez se ai comment
    hibák kijavítása

--- main.cpp
+++ main.cpp
@@ -4,24 +4,25 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // hiányzó "_"
-    std::cout << '1-100 ertekek duplazasa' // '' helyett ""
-    for (int i = 0;) //hiányos ciklusmag
+    int *b = new int[N_ELEMENTS]; // hiányzó "_"
+    std::cout << "1-100 ertekek duplazasa" // '' helyett ""
+    for (int i = 0; i<100;i++) //hiányos ciklusmag
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:" //hiányzó ";"
+        std::cout << "Ertek:"; //hiányzó ";"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
     {
-        atlag += b[i] //hiányzó ";"
+        atlag += b[i]; //hiányzó ";"
     }
     atlag /= N_ELEMENTS; 
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
 }
+
 
