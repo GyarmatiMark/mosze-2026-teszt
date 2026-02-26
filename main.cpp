52ddc3efece47219ac31446dc5a364c40b3193a0
GyarmatiMark <gyarmati.mark22@gmail.com>
2026-02-26 21:13:16 +0100

    mégtöbb hiba javítás
    talán utolsó

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ constexpr int N_ELEMENTS = 100;
 int main()
 {
     int *b = new int[N_ELEMENTS]; // hiányzó "_"
-    std::cout << "1-100 ertekek duplazasa" // '' helyett "" és ; a végére
-    for (int i = 0; i<100;i++) //hiányos ciklusmag
+    std::cout << "1-100 ertekek duplazasa"; // '' helyett "" és ; a végére
+    for (int i = 0; i<N_ELEMENTS; i++) //hiányos ciklusmag
     {
         b[i] = i * 2;
     }
@@ -15,15 +15,16 @@ int main()
         std::cout << "Ertek:"; //hiányzó ";"
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag; // nincs lenullázva
-    for (int i = 0; i < N_ELEMENTS, i++) //, helyett ;
+    int atlag = 0; // nincs lenullázva
+    for (int i = 0; i < N_ELEMENTS; i++) //, helyett ;
     {
         atlag += b[i]; //hiányzó ";"
     }
-    atlag /= N_ELEMENTS; 
+    atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
 }
+
 
 
 
